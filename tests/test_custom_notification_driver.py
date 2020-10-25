6f50bda026490f5b580b4d099f9001d84c554e2b
Samuel Girardin <samuelgirardin@pm.me>
2020-10-25 18:11:56 +0100

    complete tests for custom driver registering

--- tests/test_custom_notification_driver.py
+++ tests/test_custom_notification_driver.py
@@ -3,7 +3,6 @@ from unittest.mock import MagicMock
 
 from .UserTestCase import UserTestCase
 from src.masonite.notifications import Notification, NotificationContract
-from src.masonite.notifications.components import MailComponent
 from src.masonite.notifications.exceptions import InvalidNotificationType
 
 
@@ -23,6 +22,15 @@ class WelcomeNotification(Notification):
         return ["voice"]
 
 
+class TestCustomDriverNotRegistered(UserTestCase):
+
+    def test_sending_should_fail_when_driver_not_registered(self):
+        user = self.user()
+        with self.assertRaises(InvalidNotificationType) as e:
+            user.notify(WelcomeNotification())
+        self.assertIn("voice notification driver has not been found", str(e.exception))
+
+
 class TestCustomDriver(UserTestCase):
     """Test that custom notifications drivers can be used in an application."""
 
@@ -37,42 +45,6 @@ class TestCustomDriver(UserTestCase):
         user.notify(notification)
         NotificationVoiceDriver.send.assert_called_with(user, notification)
 
-    # def test_sending_to_anonymous_user(self):
-    #     self.notification.route(NotificationVoiceClass, "33456712780").notify(WelcomeNotification())
-    #     NotificationVoiceClass.send.assert_called()
-
-    # def test_can_send_to_multiple_drivers_with_custom(self):
-    #     class CustomNotification(Notification):
-
-    #         def to_mail(self, notifiable):
-    #             return MailComponent().line("Welcome email")
-
-    #         def via(self, notifiable):
-    #             return ["mail", NotificationVoiceClass]
-
-    #     user = self.user()
-    #     notification = WelcomeNotification()
-    #     user.notify(notification)
-    #     # check custom notif is sent
-    #     NotificationVoiceClass.send.assert_called_with(user, notification)
-
-    # def test_that_class_should_implements_send(self):
-    #     class NotificationCustomClass(NotificationContract):
-    #         pass
-    #     class WelcomeNotification(Notification):
-    #         def via(self, notifiable):
-    #             return [NotificationCustomClass]
-    #     user = self.user()
-    #     with self.assertRaises(TypeError):
-    #         user.notify(WelcomeNotification())
-
-    # def test_that_class_should_implements_notification_contract(self):
-    #     class NotificationCustomClass():
-    #         def send(self, notifiable, notification):
-    #             pass
-    #     class WelcomeNotification(Notification):
-    #         def via(self, notifiable):
-    #             return [NotificationCustomClass]
-    #     user = self.user()
-    #     with self.assertRaises(InvalidNotificationType):
-    #         user.notify(WelcomeNotification())
+    def test_sending_to_anonymous_user(self):
+        self.notification.route("voice", "33456712780").notify(WelcomeNotification())
+        NotificationVoiceDriver.send.assert_called()
