d00712340b2b0d533865bc2d6a27652cec31c0a9
Samuel Girardin <samuelgirardin@pm.me>
2020-10-25 18:44:46 +0100

    update setup and makefile

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
     name='masonite-notifications',
     package_dir={'': 'src'},
@@ -12,13 +16,23 @@ setup(
         'masonite.notifications.commands',
         'masonite.notifications.models',
     ],
-    version='2.0.0',
-    install_requires=[],
-    description='Masonite Notifications for the Masonite framework',
+    version='3.0.0',
+    install_requires=[
+        "masonite"
+    ],
+    description='Masonite Notifications Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Joseph Mancuso',
     author_email='joe@masoniteproject.com',
     url='https://github.com/MasoniteFramework/notifications',
-    keywords=['python web framework', 'python3', 'masonite'],
+    license="MIT",
+    keywords="Masonite, Python",
     classifiers=[],
     include_package_data=True,
+    extras_require={
+        "test": ["coverage", "pytest", "pytest-cov", "coveralls", "responses"],
+        "dev": ["black", "flake8", "twine>=1.5.0", "wheel"],
+        "services": ["pusher", "ably", "vonage"]
+    },
 )
